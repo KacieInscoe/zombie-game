6bd0947fd7b1d376ee89f0643bed1f9ad1aae65d
KacieInscoe <kacie.inscoe@bsu.edu>
2024-11-01 11:00:42 -0400

    Update kacie_ zombie_game.py

--- kacie_ zombie_game.py
+++ kacie_ zombie_game.py
@@ -1,34 +1,53 @@
-import pygame, simpleGE, random
-#zombie brain catcher
-class Zombie(simpleGE.Sprite):
-    def __int__(self, scene):
+import pygame, random, simpleGE
+
+#zombie catcher
+
+class brain(simpleGE.Sprite):
+    def __init__(self, scene):
         super().__init__(scene)
-        self.image("zombie.png")
-        self.setsize(50,50)
-        self.position = (320,400)
+        self.setImage("brian.png")
+        self.setSize(25, 25)
+        self.reset()
+        
+    def reset(self):
+        self.y = 10
+        self.x = random.randint(0, self.screenWidth)
+        self.dy = random.randint(3, 8)
+        
+    def checkBounds(self):
+        if self.bottom > self.screenHeight:
+            self.reset()
+
+
+class zombie(simpleGE.Sprite):
+    def __init__(self, scene):
+        super().__init__(scene)
+        self.setImage("zombie.png")
+        self.setSize(50, 50)
+        self.position = (320, 400)
         self.moveSpeed = 5
         
-    def process(self):
+         def process(self):
         if self.isKeyPressed(pygame.K_LEFT):
             self.x -= self.moveSpeed
         if self.isKeyPressed(pygame.K_RIGHT):
             self.x += self.moveSpeed
-class Brain(simpleGE.Sprite):
-    def __init__(self, scene):
-        super().__init__(scene)
-        self.setImage("brain.png")
-        self.setSize (25,25)
-        self.minSpeed = 3
-        self.maxspeed = 8
-        self.reset()
         
-   def reset (self):
-        self.x = random.randint (0, self.screenWidth)
-        self.y = 0
-        self.dy = random.randint(self.minSpeed, self.maxSpeed)
-        
-    #def checkbounds(self):
-        #if self
-class game(simpleGE.Sprite):
-    def __int__(self):
+
+class Game(simpleGE.Scene):
+    def __init__(self):
         super().__init__()
+        self.setImage("train.jpg")
+        
+        self.zombie = zombie(self)
+        self.brain = brain(self)
+        
+        self.sprites = [self.zombie,
+                        self.brain]
+        
+def main():
+    game = Game()
+    game.start()
+    
+if __name__ == "__main__":
+    main()
